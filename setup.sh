b1910c011eaa75ab48256061ca439618b8ab5b05
westerncoastal <info@loqoo.delivery>
2026-04-29 03:31:59 -0400

    Modify setup.sh for docker-compose-plugin and SSH clone
    Updated setup script to use docker-compose-plugin and SSH for cloning the repository. Added additional directories for ads and ssai.

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 set -e
 
 apt update
-apt install -y docker.io docker-compose git
+apt install -y docker.io docker-compose-plugin git
 
 systemctl enable docker
 systemctl start docker
 
-# clone repo
-git clone https://github.com/YOURNAME/stream-stack.git /opt/stack
+# clone repo (SSH version)
+git clone git@github.com:westerncoastal/stream-stack.git /opt/stack
 
 cd /opt/stack
 
-mkdir -p hls
+mkdir -p hls ads ssai
 
 docker compose up -d
